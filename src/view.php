880093120d5848245698d473abae85d70d193a93
Do-vile <dovilikezzzs@gmail.com>
2026-01-24 20:39:15 +0200

    Update view.php

--- src/view.php
+++ src/view.php
@@ -1,1 +1,32 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/auth.php';
+
+function flash_set(string $type, string $message): void
+{
+    start_session();
+    $_SESSION['flash'] = [
+        'type' => $type,
+        'message' => $message,
+    ];
+}
+
+function flash_get(): ?array
+{
+    start_session();
+    $flash = $_SESSION['flash'] ?? null;
+    unset($_SESSION['flash']);
+
+    return is_array($flash) ? $flash : null;
+}
+
+function render(string $view, array $params = []): void
+{
+    $flash = flash_get();
+    $user = current_user();
+
+    extract($params, EXTR_SKIP);
+    require __DIR__ . '/../views/' . $view . '.php';
+}
 
