df437cb9a0fb13f66c428b6a5a20fb852e594564
Do-vile <dovilikezzzs@gmail.com>
2026-01-30 18:37:38 +0200

    Update router.php

--- public/router.php
+++ public/router.php
@@ -1,4 +1,15 @@
 <?php
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $path;
+
+// 🔴 LEIDŽIAM STATINIAMS FAILAMS EITI TIESIAI
+if ($path !== '/' && file_exists($file) && !is_dir($file)) {
+    return false;
+}
+
+require __DIR__ . '/index.php';
+<?php
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $file = __DIR__ . $path;
 
