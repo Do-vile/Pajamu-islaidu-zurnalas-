228028f9ac46693b634dec2f28f06141889915e9
Do-vile <dovilikezzzs@gmail.com>
2026-01-30 19:33:16 +0200

    Update router.php

--- public/router.php
+++ public/router.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
 $file = __DIR__ . $path;
 
-// Jei prašomas realus failas public/ kataloge (css, js, img) – atiduodam jį tiesiai
 if ($path !== '/' && is_file($file)) {
-    return false;
+    return false; // leisti atiduoti css/js/img failą
 }
 
-// Visa kita – per pagrindinį app entrypoint
 require __DIR__ . '/index.php';
