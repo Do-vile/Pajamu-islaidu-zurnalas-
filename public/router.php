2ed2b3f8ea295a3555a48efb1af8621399f9b3f3
Do-vile <dovilikezzzs@gmail.com>
2026-01-30 17:50:29 +0200

    Update router.php

--- public/router.php
+++ public/router.php
@@ -1,11 +1,10 @@
 <?php
-declare(strict_types=1);
-
-$path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $file = __DIR__ . $path;
 
 if ($path !== '/' && is_file($file)) {
-    return false;
+    return false; // leisti PHP built-in serveriui atiduoti failą (CSS, JS, img)
 }
 
 require __DIR__ . '/index.php';
+
